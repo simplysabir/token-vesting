0f49fc4e7e353531b6620acca4234f39b156e8f2
simplysabir <simplysabir@gmail.com>
2023-10-02 15:38:31 +0530

    till entry point

---
+++ programs/token-vesting/src/instructions/mod.rs
@@ -0,0 +1,5 @@
+pub mod escrow_creation;
+pub mod escrow_withdraw;
+
+pub use escrow_creation::*;
+pub use escrow_withdraw::*;
--- programs/token-vesting/src/lib.rs
+++ programs/token-vesting/src/lib.rs
@@ -1,13 +1,36 @@
 use anchor_lang::prelude::*;
 
+pub mod error;
+pub mod state;
+pub mod instructions;
+
+use instructions::*;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod token_vesting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    // create an escrow
+    pub fn escrow_creation(
+        ctx: Context<EscrowCreationContext>,
+        id: Pubkey,
+        amount: u64,
+        time: u64,
+        withdrawer: Pubkey,
+    ) -> Result<()>{
+        escrow_creation::handler(ctx, id, amount, time, withdrawer)
+    }
+
+    // Withdraw from an escrow
+    pub fn escrow_withdraw(
+        ctx: Context<EscrowWithdrawContext>,
+        id: Pubkey,
+        escrow_owner: Pubkey,
+        token_bump: u8,
+    ) -> Result<()> {
+        escrow_withdraw::handler(ctx, id, escrow_owner, token_bump)
     }
 }
 
